bcbd31dfea9ee5704b9471080395d809ed14205b
trzbh <148909808+trzbh@users.noreply.github.com>
2025-11-20 15:52:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,6 @@ document.getElementById('contato-form').addEventListener('submit', function(e) {
     alert("Mensagem enviada com sucesso!");
     this.reset();
 });
-
-// Mostrar/ocultar redações
 function toggleRedacao(id) {
     const balao = document.getElementById(id);
 
@@ -25,18 +23,3 @@ function toggleRedacao(id) {
     balao.style.display = balao.style.display === "block" ? "none" : "block";
 }
 
-// Cabeçalho que some ao rolar para baixo e reaparece ao subir
-let prevScrollpos = window.pageYOffset;
-const header = document.querySelector("header");
-
-window.onscroll = function() {
-    let currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        header.style.top = "0"; // rolando para cima → mostra header
-    } else {
-        header.style.top = "-100px"; // rolando para baixo → esconde header
-    }
-    prevScrollpos = currentScrollPos;
-};
-
-};
